4cc556d9cbe6f985ceb58ecc656273dc3ba36649
Charlie's Tablet <charlie.parsons@students.plymouth.ac.uk>
2019-12-24 23:20:59 +0000

    Login JFrame and New User JFrame Added

--- PatientManagementSystem/src/patientmanagementsystem/PatientManagementSystem.java
+++ PatientManagementSystem/src/patientmanagementsystem/PatientManagementSystem.java
@@ -5,6 +5,8 @@
  */
 package patientmanagementsystem;
 
+import GUIs.Login;
+
 /**
  *
  * @author Charlie Parsons
@@ -15,7 +17,8 @@ public class PatientManagementSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login LoginScreen = new Login();
+        LoginScreen.setVisible(true);
     }
     
 }
